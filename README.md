18df7673aa874207764dc55dece0d0816cb79e1a
James Wen <jrw2175@columbia.edu>
2014-11-22 23:18:11 -0500

    Created README and initialized GroupMe API Interface

---
+++ README.md
@@ -0,0 +1,13 @@
+SayWhat
+------
+
+####Functionality:
+
+Predicts text output for various social circuits like GroupMe, Twitter, Facebook, etc. given access to previous text material (corpus).
+
+####Technical/Theoretical Considerations:
+
+GroupMe:
+- Complete sentences might be broken up into multiple messages
+- No ending punctuation for most sentences probably (informal typing/text)
+
---
+++ groupme.py
@@ -0,0 +1,19 @@
+import requests
+
+class GroupMeInterface:
+  api_url = "https://api.groupme.com/v3/"
+
+  @staticmethod  
+  def get_groups(access_token):
+
+  @staticmethod  
+  def get_group(access_token, group_id):
+
+  @staticmethod  
+  def get_all_messages(access_token, group_id):
+
+  @staticmethod  
+  def get_messages(access_token, group_id):
+
+  @staticmethod  
+  def get_user_info(access_token): 
