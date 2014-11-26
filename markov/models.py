eb627fd3b810f9da3509492a3f3df67bd7645f2f
James Wen <jrw2175@columbia.edu>
2014-11-26 18:01:28 -0500

    Initialized routes and index page for markov app with template serving

--- markov/models.py
+++ markov/models.py
@@ -4,3 +4,5 @@ class User(models.Model):
   groupme_uid = models.CharField(max_length=100)
   access_token = models.CharField(max_length=100)
   name = models.CharField(max_length=200)
+  def __str__(self):  
+    return "{} {}".format(self.groupme_uid, self.name)
--- markov/views.py
+++ markov/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from markov.models import User
+
+def index(request):
+  template = loader.get_template('markov/index.html')
+  context = RequestContext(request, {
+  })
+  return HttpResponse(template.render(context))
