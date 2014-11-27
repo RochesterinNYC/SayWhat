72af61a89f5adc5b2f4dff98e0a6f566f85cda2c
James Wen <jrw2175@columbia.edu>
2014-11-27 12:54:40 -0500

    Fixing dj database url hash

--- SayWhat/settings.py
+++ SayWhat/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
