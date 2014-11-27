70e5e1e386f5bf9343eb8e3898d11b93f78204af
James Wen <jrw2175@columbia.edu>
2014-11-27 13:43:51 -0500

    Added configuration and versioning for dj-static

--- SayWhat/settings.py
+++ SayWhat/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 import dj_database_url
@@ -88,14 +88,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(
-        os.path.dirname(__file__),
-        'static',
-    ),
+    os.path.join(BASE_DIR, 'static'),
 )
-
 
 GROUPME_API_TOKEN = os.environ.get('GROUPME_API_TOKEN')
